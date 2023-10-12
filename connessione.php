47ebc5b36d8a50a77ae9ecacfa3b5997167eee31
spar-git <stefano92lt@libero.it>
2023-10-12 11:19:21 +0200

    Update connessione.php

---
+++ connessione.php
@@ -0,0 +1,16 @@
+<?php
+$db_name = "EcommerceDB";                       //database 
+$STuser_table_name = "STuser";                  //tabella utenti
+$STmovie_table_name = "STmovie";                //tabella film
+$STmusic_table_name = "STmusic";                //tabella musica
+$STrecensioni_table_name = "STrecensioni";      //tabella recensioni
+
+//tentativo connessione
+$mysqliConnection = new mysqli("localhost", "archer", "archer", $db_name);      //tentativo connessione
+
+// controllo della connessione
+if (mysqli_connect_errno()) {
+    printf("Abbiamo problemi con la connessione al db: %s\n", mysqli_connect_error($mysqliConnection));
+    exit();
+}
+?>
