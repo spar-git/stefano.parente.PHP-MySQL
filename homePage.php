d1a24c184306a58c3c51bc4846eb799588fa77ed
spar-git <stefano92lt@libero.it>
2023-10-23 12:16:22 +0200

    Update homePage.php

---
+++ homePage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="mieistili.css">
+	<title>Home Page di MediaShop Online</title>
+</head>
+
+<body style="font-family: Arial, sans-serif;">
+<?php require_once("./menù.html");?>
+<hr />
+<h1 style="text-align: center; font-size: 36px; margin-top: 200px;">Benvenuti nel nostro negozio online di Musica e Film!</h1>
+<img style="display: block; margin: 0 auto;" src="homeimg.jpg" alt="Immagine benvenuto">
+<section>
+    <p>Esplora il nostro vasto catalogo di musica e film di alta qualità, pronto per essere scaricato e goduto nella comodità della tua casa. Siamo appassionati di cultura musicale e cinematografica e offriamo una vasta gamma di opzioni per soddisfare i tuoi gusti e preferenze. Se sei un amante della musica, troverai brani di tutti i generi, dagli ultimi successi alle perle nascoste. Se sei un cinefilo, abbiamo film per ogni genere e epoca.</p>
+    <p><strong>Perché scegliere il nostro negozio online?</strong></p>
+</section>
+
+<section>
+    <img style="width: 200px;"src="musicandfilm.jpg" alt="musica e film">
+    <p style="text-align: left;">
+		Vasta Selezione: Scegli tra migliaia di brani musicali e film, dai classici senza tempo alle ultime uscite.
+	</p>
+</section>
+
+
+
+
+<section>
+    	<ul style="list-style: disc; padding-left: 20px;">
+        	<li>Vasta Selezione: Scegli tra migliaia di brani musicali e film, dai classici senza tempo alle ultime uscite.</li>
+        	<li>Facilità di Utilizzo: Il nostro sito web è progettato per rendere la ricerca e il download semplici e intuitivi.</li>
+        	<li>Qualità Superiore: Offriamo contenuti ad alta risoluzione e audio di qualità impeccabile per un'esperienza di intrattenimento senza compromessi.</li>
+        	<li>Sicurezza e Affidabilità: I tuoi dati e le tue transazioni sono al sicuro con noi. Garantiamo una piattaforma sicura per il tuo shopping online.</li>
+        	<li>Supporto Clienti: Il nostro team di supporto è disponibile per rispondere alle tue domande e assisterti in ogni fase dell'acquisto.</li>
+    	</ul>
+    <p>Inizia a esplorare ora e porta a casa la musica e i film che ami. Grazie per aver scelto il nostro negozio online e buon shopping!</p>
+</section>
+</body>
+</html>
