ef1e1e9b559099bb93424f60372803c6017958a9
spar-git <stefano92lt@libero.it>
2023-10-23 12:16:25 +0200

    Update login.php

--- login.php
+++ login.php
@@ -40,7 +40,8 @@ if (isset($_POST['invio']))                                         //è stato d
 ?>
 
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+<!DOCTYPE html 
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
