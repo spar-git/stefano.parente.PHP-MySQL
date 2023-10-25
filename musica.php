588e186e30d616e9a586d0996bf13a60177f9feb
spar-git <stefano92lt@libero.it>
2023-10-25 12:11:56 +0200

    Update musica.php

---
+++ musica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="mieistili.css">
+	<title>Scarica la tua musica</title>
+</head>
+
+<body>
+<?php require_once("./menù.html"); ?>
+<hr />
+
+<div style="margin-top: 120px"> 
+    <p class="path"><a href="homePage.php">Home</a> / Musica</p>
+</div>
+
+</body>
+</html>
