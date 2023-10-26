b7bb32975fff2bf4ebf199b7ae700f9716deaaff
spar-git <stefano92lt@libero.it>
2023-10-26 12:11:48 +0200

    Update negozio.php

---
+++ negozio.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="mieistili.css">
+	<title>il negozio</title>
+</head>
+
+<body>
+<?php require_once("./menù.html"); ?>
+<hr />
+
+<div> 
+    <p class="path"><a href="homePage.php">Home</a> / Negozio</p>
+</div>
+
+<div>
+	<h2 style="display: inline;">Il negozio</h2>
+    <h4 style="font-style: italic;display: inline;">di Media Shop Online</h3>
+</div>
+
+<div>
+    <p>Siamo appassionati di musica e cinema, e vogliamo condividere con te una vasta selezione di brani musicali, 
+        album e film da scaricare. Il nostro obiettivo è rendere l'arte e l'intrattenimento accessibili a tutti. Unisciti 
+        a noi nella scoperta di un mondo di emozioni e intrattenimento digitali.
+    </p>
+</div>
+
+<div>
+	<div class="box-assistenza">
+    	<div>
+            
+    	</div>
+
+		<div>
+
+    	</div>
+</div>
+
+<div>
+    <div class="dove-siamo-box">
+        <h2>Dove Siamo</h2>
+        <p>La sede di Media Shop Online si trova in Via A. Doria, 23, a Latina. Per raggiungerci, segui le seguenti indicazioni:</p>
+        <ol>
+            <li>Dalla stazione ferroviaria di Latina, svoltare a sinistra e camminare per 200 metri.</li>
+            <li>Presso l'incrocio di Via A. Doria, svoltare a destra.</li>
+            <li>Prosegui per altri 100 metri e troverai la nostra sede sulla sinistra, al numero 23.</li>
+        </ol>
+        <p>Non esitare a contattarci se hai bisogno di ulteriori informazioni o indicazioni.</p>
+    </div>
+</div>
