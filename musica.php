4cc138192b84837c5a0afaa502913f2538abe7d5
spar-git <stefano92lt@libero.it>
2023-10-26 12:11:45 +0200

    Update musica.php

--- musica.php
+++ musica.php
@@ -11,8 +11,48 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <?php require_once("./menù.html"); ?>
 <hr />
 
-<div style="margin-top: 120px"> 
+<div> 
     <p class="path"><a href="homePage.php">Home</a> / Musica</p>
+</div>
+
+<div>
+	<h2>Musica</h2>
+</div>
+
+<div>
+	<div class="box-prodotti">
+    	<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>autore 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>autore 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>autore 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>autore 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+	</div>
 </div>
 
 </body>
