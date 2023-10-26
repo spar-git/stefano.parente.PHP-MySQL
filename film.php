9889c7bdade9a90e5a0d0974afccd8fb08561271
spar-git <stefano92lt@libero.it>
2023-10-26 12:11:35 +0200

    Update film.php

---
+++ film.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="mieistili.css">
+	<title>Scarica i tuoi film</title>
+</head>
+
+<body>
+<?php require_once("./menù.html"); ?>
+<hr />
+
+<div> 
+    <p class="path"><a href="homePage.php">Home</a> / Film</p>
+</div>
+
+<div>
+	<h2>Film</h2>
+</div>
+
+<div>
+	<div class="box-prodotti">
+    	<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>genere 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>genere 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>genere 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+
+		<div class="prodotti">
+        	<h2>titolo 1</h2>
+        	<p>genere 1</p>
+			<p>numero voti</p>
+			<h3>prezzo</h3>
+        	<input class="button" type="submit" name="invio" value="Aggiungi al carrello">
+    	</div>
+	</div>
+</div>
+
+</body>
+</html>
