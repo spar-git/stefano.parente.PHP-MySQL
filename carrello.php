bc9c8f787c35e90eb3264b0f87e5eafcda80c809
spar-git <stefano92lt@libero.it>
2023-11-21 10:13:28 +0100

    Update carrello.php

--- carrello.php
+++ carrello.php
@@ -33,9 +33,8 @@ if (isset($_POST["invia_recensione"])){
 		
 		if ($valutazione>3) 
 			$valutazione= "Fantastico! Siamo lieti che tu abbia apprezzato il nostro lavoro, a presto!";
-		
-		if ($valutazione<=3)
-			$valutazione= "Siamo spiacenti per la tua esperienza negativa. Segnalaci il tuo problema a info@msonline.it e proveremo ad aiutarti!";  
+		else
+			$valutazione= "Siamo spiacenti per la tua esperienza negativa. Segnalaci il problema a info@msonline.it e proveremo ad aiutarti!";  
 		
 	}
 }
@@ -77,7 +76,7 @@ if (isset($_POST["acquista"])){
     		<input type="radio" name="stelle" value="2"> 2
     		<input type="radio" name="stelle" value="3"> 3
     		<input type="radio" name="stelle" value="4"> 4
-    		<input type="radio" name="stelle" value="5"> 5
+    		<input type="radio" name="stelle" value="5" checked> 5
 			</p>
 			<p>Titolo: <textarea name="titolo" maxlength="20" rows="1" cols="30"></textarea><br></p>
 			<p>Descrizione:<br><textarea name="descrizione" maxlength="250" rows="4" cols="100"></textarea><br></p>
@@ -153,29 +152,18 @@ if (!empty($_SESSION["carrello_movie"])) {
 }
 
 if ($flag!=1){
-
-echo "<div><p>
-		<span class=\"totale\"><strong>Totale</strong></span>";
-		$sconto=1;
-		if (isset($_SESSION['puntiFedeltà'])){
-			if ($_SESSION['puntiFedeltà']>=20){
-				$sconto=0.80;
-				echo "<span class=\"price\"><strong><strike>" . $totale . " €</strike></strong><strong>" . $totale*$sconto . " €</strong></span>";
-			} else {
-				echo" <span class=\"price\"><strong>" . $totale . " €</strong></span>";
-			}
-		} else {
-		echo "<span class=\"price\"><strong>" . $totale . " €</strong></span>";
-		}
-		if ($totale>0) { 
-		echo "<form action=" . $_SERVER['PHP_SELF'] . " method=\"post\">
-			<input class=\"button\" type=\"submit\" name=\"acquista\" value=\"Acquista\">
-		</form>";
-		}
+	echo "<div><p>";
+	if ($totale>0) { 
+		echo "<span class=\"totale\"><strong>Totale</strong></span>
+			<span class=\"price\";><strong>" . $totale ." €</strong></span>
+			<form action=" . $_SERVER['PHP_SELF'] . " method=\"post\">
+				<input class=\"button\" type=\"submit\" name=\"acquista\" value=\"Acquista\">
+			</form>
+		</p></div>";
+	}
 } 
 ?>
-</p></div>
 </div>
-			
+		
 </body>
 </html>
