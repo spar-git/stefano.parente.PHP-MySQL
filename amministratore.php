9d6d07725714bcc06963dcec455610f68fb55029
spar-git <stefano92lt@libero.it>
2023-11-25 12:45:47 +0100

    Update amministratore.php

--- amministratore.php
+++ amministratore.php
@@ -12,8 +12,10 @@ if(isset($_SESSION['accessoPermesso'])) {        // impedisce l'accesso a chi no
     }
 }
 
-require_once("./connessione.php");
-$mysqliConnection = new mysqli("localhost", "root", "pass123", $db_name);
+require_once("./setup/connessione.php");
+if (!$mysqliConnection->select_db($db_name)) {
+    echo "Errore nella selezione del database: " . $mysqliConnection->error;
+}
 
 $esito="";
 
@@ -132,7 +134,7 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="mieistili.css">
+    <link rel="stylesheet" type="text/css" href="style/mieistili.css">
 	<title>amministratore</title>
 </head>
 <body>
